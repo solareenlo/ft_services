6b81a20a885dbd827e1499f478cb3067c6897e0c
solareenlo <solareenlo@gmail.com>
2020-12-07 17:27:08 +0900

    Add: nginx

--- setup.sh
+++ setup.sh
@@ -6,8 +6,40 @@
 #    By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/28 14:34:11 by tayamamo          #+#    #+#              #
-#    Updated: 2020/11/28 14:34:15 by tayamamo         ###   ########.fr        #
+#    Updated: 2020/12/07 09:22:16 by tayamamo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
+
+PODS=(nginx)
+
+function deploy()
+{
+	docker build -t my_$1 ./srcs/$1
+}
+
+# Colors
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RESET='\033[0;0m'
+
+echo "${GREEN}Starting minikube...${RESET}"
+
+# delete minikube local status
+# minikube delete
+
+# start minikube
+# minikube start --driver=virtualbox
+
+# To point your shell to minikube's docker-daemon
+# eval $(minikube -p minikube docker-env)
+
+for image in "${PODS[@]}"
+do
+	# echo $image
+	deploy $image
+done
+
+# open dashboard
+# minikube dashboard

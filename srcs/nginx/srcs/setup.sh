5b0f40ff9f66f6571734869ccc683b213dcf8316
solareenlo <solareenlo@gmail.com>
2020-12-12 13:19:28 +0900

    Fix: nginx

--- srcs/nginx/srcs/setup.sh
+++ srcs/nginx/srcs/setup.sh
@@ -6,7 +6,7 @@
 #    By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/08 19:13:13 by tayamamo          #+#    #+#              #
-#    Updated: 2020/12/09 21:27:27 by tayamamo         ###   ########.fr        #
+#    Updated: 2020/12/12 12:44:36 by tayamamo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,13 +16,13 @@
 # telegraf &
 
 # Create new user without creating a home directory
-adduser -D "$SSH_USER"
+# adduser -D "$SSH_USER"
 # Update password for the new user
-echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
+# echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
 
 # sshd is the daemon program for ssh connection
-/usr/sbin/sshd -D
+# /usr/sbin/sshd
 
 # For normal prduction (on a server), use the default daemon on;
 # In this case of Docker containers(or for debugging), use the daemon off;
-nginx -g "daemon off;"
+/usr/sbin/sshd; nginx -g "daemon off;"

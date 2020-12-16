3aecd29d59dfbd767378d623e75b7db0e25ca44a
solareenlo <solareenlo@gmail.com>
2020-12-17 03:23:05 +0900

    Update: nginx

--- srcs/nginx/srcs/setup.sh
+++ srcs/nginx/srcs/setup.sh
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    setup.sh                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/12/08 19:13:13 by tayamamo          #+#    #+#              #
-#    Updated: 2020/12/12 12:44:36 by tayamamo         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!/bin/sh
 
 # Start telegraf
@@ -20,9 +8,11 @@
 # Update password for the new user
 # echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
 
+/usr/sbin/php-fpm7
+
 # sshd is the daemon program for ssh connection
-# /usr/sbin/sshd
+/usr/sbin/sshd
 
 # For normal prduction (on a server), use the default daemon on;
 # In this case of Docker containers(or for debugging), use the daemon off;
-/usr/sbin/sshd; nginx -g "daemon off;"
+nginx -g "daemon off;"

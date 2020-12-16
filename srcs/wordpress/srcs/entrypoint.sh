08aa139acca2591d263009dd209d519d8ce592d8
solareenlo <solareenlo@gmail.com>
2020-12-17 03:22:40 +0900

    Update: wordpress

--- srcs/wordpress/srcs/entrypoint.sh
+++ srcs/wordpress/srcs/entrypoint.sh
@@ -3,11 +3,11 @@
 # terminate on errors
 set -e
 
-# PHP_CGI_FIX_PATHINFO=0
+PHP_CGI_FIX_PATHINFO=0
 # DATABASE_NAME=wordpress
 # USERNAME=username
 # PASSWORD=password
-# WORDPRESS_SERVICE_HOST=wordpress-mysql
+# WORDPRESS_DB_HOST=wordpress-mysql
 
 # Configure PHP
 sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo=$PHP_CGI_FIX_PATHINFO|i" /etc/php7/php.ini
@@ -16,7 +16,7 @@ sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo=$PHP_CGI_FIX_PATHINFO|i" /etc/p
 sed -i "s/database_name_here/$DATABASE_NAME/" /usr/src/wordpress/wp-config.php
 sed -i "s/username_here/$USERNAME/" /usr/src/wordpress/wp-config.php
 sed -i "s/password_here/$PASSWORD/" /usr/src/wordpress/wp-config.php
-sed -i "s/localhost/$WORDPRESS_SERVICE_HOST/" /usr/src/wordpress/wp-config.php
+sed -i "s/localhost/$WORDPRESS_DB_HOST/" /usr/src/wordpress/wp-config.php
 sed -i "s/utf8/utf8mb4/" /usr/src/wordpress/wp-config.php
 
 # Generate secret-keys
@@ -27,24 +27,23 @@ if [ ! "$(ls -A "/var/www/wp-content" > /dev/null 2>&1)" ]; then
     # Copy wp-content from Wordpress src to volume
     cp -r /usr/src/wordpress/wp-content /var/www/
     chown -R nobody:nobody /var/www
-
 fi
 
 # Wait until mysql is up
 # until sudo -u www wp db check --path="/www"; do echo waiting for mysql; sleep 2; done
 
-cat > /tmp/sql << EOF
-CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;
-CREATE USER '$USERNAME'@'$WORDPRESS_SERVICE_HOST' IDENTIFIED BY '$PASSWORD';
-GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$USERNAME'@'$WORDPRESS_SERVICE_HOST';
-EOF
+# cat > /tmp/sql << EOF
+# CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;
+# CREATE USER '$USERNAME'@'$WORDPRESS_DB_HOST' IDENTIFIED BY '$PASSWORD';
+# GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$USERNAME'@'$WORDPRESS_DB_HOST';
+# EOF
 
 # mysql --host=$WORDPRESS_DB_HOST --user=root --password=$MYSQL_ROOT_PASSWORD < /tmp/sql
 # rm /tmp/sql
 
-adduser --no-create-home -D $USERNAME
-echo "$USERNAME:$PASSWORD" | chpasswd
-chmod -R 755 /usr/src/
-chown -R $USERNAME:$USERNAME /usr/src/wordpress
+# adduser --no-create-home -D $USERNAME
+# echo "$USERNAME:$PASSWORD" | chpasswd
+# chmod -R 755 /usr/src/
+# chown -R $USERNAME:$USERNAME /usr/src/wordpress
 
 exec "$@"

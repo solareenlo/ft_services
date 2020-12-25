3a1e94af39185a8f37a33f3b2b1ab76c5a8dc404
solareenlo <solareenlo@gmail.com>
2020-12-25 17:19:01 +0900

    Add: grafana and telegraf users for influxdb

--- srcs/influxdb/srcs/setup.sh
+++ srcs/influxdb/srcs/setup.sh
@@ -12,6 +12,12 @@ if [ -z "$(ls -A -- /var/lib/influxdb)" ]; then
     # Authenticate as admin
     INFLUX_USERNAME=${INFLUXDB_ADMIN_USERNAME}
     INFLUX_PASSWORD=${INFLUXDB_ADMIN_PASSWORD}
+    # Create telegraf user with write privileges
+    influx -execute "CREATE USER \"${INFLUXDB_TELEGRAF_USERNAME}\" WITH PASSWORD '${INFLUXDB_TELEGRAF_PASSWORD}'"
+    influx -execute "GRANT WRITE ON \"${INFLUXDB_DATABASE}\" TO \"${INFLUXDB_TELEGRAF_USERNAME}\""
+    # Create grafana user with read privileges
+    influx -execute "CREATE USER \"${INFLUXDB_GRAFANA_USERNAME}\" WITH PASSWORD '${INFLUXDB_GRAFANA_PASSWORD}'"
+    influx -execute "GRANT READ ON \"${INFLUXDB_DATABASE}\" TO \"${INFLUXDB_GRAFANA_USERNAME}\""
     # Shutdown influxd
 	pkill influxd
 fi
